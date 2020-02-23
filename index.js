9023b3306bdcee44d293fd09152a1947bc64d092
fancyzhong <fancyzhong@tencent.com>
2020-02-24 01:16:17 +0800

    adap to concent 1.5.152+

--- index.js
+++ index.js
@@ -6,29 +6,26 @@ var cc = require('concent');
 var ROUTER_MODULE = require('./constant').ROUTER_MODULE;
 // var ROUTER_MODULE= Symbol('');
 
-var isCcAlreadyStartup = cc.ccContext.isCcAlreadyStartup;
-if (isCcAlreadyStartup === false) {
-  cc.ccContext.isCcAlreadyStartup = true;
-}
-cc.configure(ROUTER_MODULE, {
-  state: {
-    hash: '',
-    key: '',
-    pathname: '',
-    search: '',
-    state: null,
-  }
-});
-if (isCcAlreadyStartup === false) {
-  cc.ccContext.isCcAlreadyStartup = false;
+
+var configRouterModule = (moduleName = ROUTER_MODULE) => {
+  cc.configure(moduleName, {
+    state: {
+      hash: '',
+      key: '',
+      pathname: '',
+      search: '',
+      state: null,
+    }
+  });
 }
 
-const toExport = module.exports = {};
+var toExport = module.exports = {};
 
 toExport.ROUTER_MODULE = ROUTER_MODULE;
 toExport.ConnectRouter = ConnectRouter;
 toExport.Link = Link;
 toExport.history = historyProxy.getHistory();
 toExport.createHistoryProxy = createHistoryProxy;
+toExport.configRouterModule = configRouterModule;
 
 toExport.default = toExport;
